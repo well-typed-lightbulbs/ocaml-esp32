c7bfe862e2ba67e18f25f39b68beb0c27fad3795
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-03-12 15:05:29 +0000

    Beginning asm emission

--- asmcomp/xtensa/selection.ml
+++ asmcomp/xtensa/selection.ml
@@ -1,10 +1,22 @@
 open Arch
+open Cmm
 
 (* Recognition of addressing modes *)
 
 class selector = object (_self)
 
 inherit Selectgen.selector_generic as _super 
+
+method! regs_for tyv =
+  Reg.createv (begin
+                 (* Expand floats into pairs of integer registers *)
+                 let rec expand = function
+                   [] -> []
+                 | Float :: tyl -> Int :: Int :: expand tyl
+                 | ty :: tyl -> ty :: expand tyl in
+                 Array.of_list (expand (Array.to_list tyv))
+               end
+               )
 
 method is_immediate _ = false 
 
